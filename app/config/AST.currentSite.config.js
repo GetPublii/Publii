91ed57f919a69b658c69aaa8af9dd3eac57d2943
Tomasz Dziuda <dziudek@gmail.com>
2018-07-09 10:11:47 +0200

    #15 - added confiruation form for GitLab Pages

--- app/config/AST.currentSite.config.js
+++ app/config/AST.currentSite.config.js
@@ -122,6 +122,12 @@ const AstCurrentSiteConfig = {
             branch: '',
             token: ''
         },
+        gitlab: {
+            user: '',
+            repo: '',
+            branch: '',
+            token: ''
+        },
         netlify: {
             id: '',
             token: ''
