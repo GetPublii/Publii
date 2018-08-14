5ec1f378cf98023a6092e35e3a4fcdf45a5c8cd1
Tomasz Dziuda <dziudek@gmail.com>
2018-08-14 19:52:51 +0200

    #113 - fixed crash on error message

--- app/back-end/events/preview.js
+++ app/back-end/events/preview.js
@@ -70,7 +70,7 @@ class PreviewEvents {
                     let errorDesc = 'Checkout the rendering-errors.log and rendering-process.log files under Tools -> Log viewer. ';
                     let errorTitle = 'Rendering process crashed';
 
-                    if (data.result[0] && data.result[0].message) {
+                    if (data.result && data.result[0] && data.result[0].message) {
                         errorTitle = 'Rendering process failed';
                         errorDesc = data.result[0].message + "\n\n" + data.result[0].desc;
                     }
@@ -111,7 +111,7 @@ class PreviewEvents {
                     let errorDesc = 'Checkout the rendering-errors.log and rendering-process.log files under Tools -> Log viewer. ';
                     let errorTitle = 'Rendering process crashed';
 
-                    if (data.result[0] && data.result[0].message) {
+                    if (data.result && data.result[0] && data.result[0].message) {
                         errorTitle = 'Rendering process failed';
                         errorDesc = data.result[0].message + "\n\n" + data.result[0].desc;
                     }
