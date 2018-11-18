f364689b69a9895f8f190dd5b02bb7a1ad7398ea
Tomasz Dziuda <dziudek@gmail.com>
2018-11-18 16:27:35 +0100

    #205 - added getPosts helper

---
+++ app/back-end/modules/render-html/handlebars/helpers/get-posts.js
@@ -0,0 +1,44 @@
+/**
+ * Helper for loading posts data
+ *
+ * {{#getPosts "POST_ID_1,POST_ID_2,POST_ID_N" "prefix" "suffix"}}
+ * <article>
+ *    <h2>{{ title }}</h2>
+ *    <div>{{{ excerpt }}}</div>
+ * </article>
+ * {{/getPosts}}
+ *
+ * Posts are ordered by the ID order in the string.
+ *
+ * The second parameter creates HTML prefix, the third parameter creates HTML suffix for the generated output.
+ *
+ * IMPORTANT: It requires availability of the @website.contentStructure global variable
+ */
+function getPostsHelper(rendererInstance, Handlebars) {
+    Handlebars.registerHelper('getPosts', function (postIDs, prefix, suffix, options) {
+        if (!rendererInstance.contentStructure.posts) {
+            return 'Error: @website.contentStructure global variable is not available.';
+        }
+
+        let content = '';
+        postIDs = postIDs.split(',').map(n => parseInt(n, 10));
+
+        for (let i = 0; i < postIDs.length; i++) {
+            let postData = rendererInstance.contentStructure.posts.filter(post => post.id === postIDs[i]);
+
+            if (postData.length) {
+                content += options.fn(postData[0]);
+            }
+        }
+
+        if(content === '') {
+            return 'Error: posts with given IDs are not exits.';
+        }
+
+        content = [prefix, content, suffix].join('');
+
+        return content;
+    });
+}
+
+module.exports = getPostsHelper;
