924c7ca4a1b2f3875cac1765ac026f4007ff9d95
Tomasz Dziuda <dziudek@gmail.com>
2018-11-18 16:07:44 +0100

    #204 - added getPost helper

---
+++ app/back-end/modules/render-html/handlebars/helpers/get-post.js
@@ -0,0 +1,27 @@
+/**
+ * Helper for loading post data
+ *
+ * {{#getPost POST_ID}}
+ *    <h2>{{ title }}</h2>
+ *    <div>{{{ excerpt }}}</div>
+ * {{/getPost}}
+ *
+ * IMPORTANT: It requires availability of the @website.contentStructure global variable
+ */
+function getPostHelper(rendererInstance, Handlebars) {
+    Handlebars.registerHelper('getPost', function (postID, options) {
+        if (!rendererInstance.contentStructure.posts) {
+            return 'Error: @website.contentStructure global variable is not available.';
+        }
+
+        let postData = rendererInstance.contentStructure.posts.filter(post => post.id === postID);
+
+        if(!postData.length) {
+            return 'Error: post with given ID not exits.';
+        }
+
+        return options.fn(postData[0]);
+    });
+}
+
+module.exports = getPostHelper;
