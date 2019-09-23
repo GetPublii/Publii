a75be594eb5dff4f94584f397b135adc65cd6e44
Bob Mitro <bogmitro@gmail.com>
2019-09-23 10:14:17 +0200

    #500 UI updated

--- app/src/components/PostEditorBlockEditor.vue
+++ app/src/components/PostEditorBlockEditor.vue
@@ -336,7 +336,7 @@ export default {
                 left: 0;
                 position: absolute;
                 right: 0;
-                top: 0;
+                top: 8rem;
             }
         }
     }
