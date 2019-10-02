c53eea327979a4c176502309a2f54ad118133811
Tomasz Dziuda <dziudek@gmail.com>
2019-10-02 22:37:26 +0200

    #551 #572 - hidden app menu bar

--- app/back-end/app.js
+++ app/back-end/app.js
@@ -490,6 +490,7 @@ class App {
 
         this.mainWindow = new BrowserWindow(windowParams);
         this.mainWindow.setMenu(null);
+        this.mainWindow.removeMenu();
         this.mainWindow.loadURL('file://' + this.basedir + '/index.html');
 
         this.mainWindow.webContents.on('did-finish-load', function() {
