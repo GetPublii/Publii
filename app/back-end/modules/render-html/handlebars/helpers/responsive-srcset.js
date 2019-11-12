b2f30ec3725bdd2863101647a7498825993e521f
Tomasz Dziuda <dziudek@gmail.com>
2019-11-12 14:26:28 +0100

    #610 - removed srcset and sizes attributes from SVG images

--- app/back-end/modules/render-html/handlebars/helpers/responsive-srcset.js
+++ app/back-end/modules/render-html/handlebars/helpers/responsive-srcset.js
@@ -12,54 +12,68 @@ const URLHelper = require('./../../helpers/url.js');
  * @returns {string} - string with the srcset attribute
  */
 function responsiveSrcSetHelper(rendererInstance, Handlebars) {
-    Handlebars.registerHelper('responsiveSrcSet', function (url, group) {
-        if(!url) {
-            return;
-        }
-
-        url = URLHelper.fixProtocols(normalizePath(url));
-        let output = '';
-        let dimensions = false;
-        let dimensionsData = false;
-
-        // Check if the responsive config exists
-        if(!UtilsHelper.responsiveImagesConfigExists(rendererInstance.themeConfig)) {
-            return output;
-        }
-
-        if(typeof group !== "string") {
-            group = false;
-        }
-
-        // Check for the config
-        if(UtilsHelper.responsiveImagesConfigExists(rendererInstance.themeConfig, 'optionImages')) {
-            dimensions = UtilsHelper.responsiveImagesDimensions(rendererInstance.themeConfig, 'optionImages', group);
-            dimensionsData = UtilsHelper.responsiveImagesData(rendererInstance.themeConfig, 'optionImages', group);
-        } else if(UtilsHelper.responsiveImagesConfigExists(rendererInstance.themeConfig, 'contentImages')) {
-            dimensions = UtilsHelper.responsiveImagesDimensions(rendererInstance.themeConfig, 'contentImages');
-            dimensionsData = UtilsHelper.responsiveImagesData(rendererInstance.themeConfig, 'contentImages');
-        }
-
-        if(!dimensions) {
-            return false;
-        }
-
-        let srcset = [];
-
-        for(let name of dimensions) {
-            let filename = url.split('/');
-            filename = filename[filename.length-1];
-            let filenameFile = path.parse(filename).name;
-            let filenameExtension = path.parse(filename).ext;
-            let baseUrlWithoutFilename = url.replace(filename, '');
-            let responsiveImage = baseUrlWithoutFilename + 'responsive/' + filenameFile + '-' + name + filenameExtension;
-            srcset.push(responsiveImage + ' ' + dimensionsData[name].width + 'w');
-        }
-
-        output = ' srcset="' + srcset.join(' ,') + '" ';
-
-        return new Handlebars.SafeString(output);
-    });
+    Handlebars.registerHelper('responsiveSrcSet', returnSrcSetAttribute);
 }
 
-module.exports = responsiveSrcSetHelper;
+function returnSrcSetAttribute (url, group, instance = false) {
+    if(!url) {
+        return;
+    }
+
+    if (instance) {
+        rendererInstance = instance;
+    }
+
+    url = URLHelper.fixProtocols(normalizePath(url));
+    let output = '';
+    let dimensions = false;
+    let dimensionsData = false;
+
+    // Check if the responsive config exists
+    if(!UtilsHelper.responsiveImagesConfigExists(rendererInstance.themeConfig)) {
+        return output;
+    }
+
+    // skip GIF and SVG images
+    if(url.slice(-4) === '.gif' || url.slice(-4) === '.svg') {
+        return output;
+    }
+
+    if(typeof group !== "string") {
+        group = false;
+    }
+
+    // Check for the config
+    if(UtilsHelper.responsiveImagesConfigExists(rendererInstance.themeConfig, 'optionImages')) {
+        dimensions = UtilsHelper.responsiveImagesDimensions(rendererInstance.themeConfig, 'optionImages', group);
+        dimensionsData = UtilsHelper.responsiveImagesData(rendererInstance.themeConfig, 'optionImages', group);
+    } else if(UtilsHelper.responsiveImagesConfigExists(rendererInstance.themeConfig, 'contentImages')) {
+        dimensions = UtilsHelper.responsiveImagesDimensions(rendererInstance.themeConfig, 'contentImages');
+        dimensionsData = UtilsHelper.responsiveImagesData(rendererInstance.themeConfig, 'contentImages');
+    }
+
+    if(!dimensions) {
+        return false;
+    }
+
+    let srcset = [];
+
+    for(let name of dimensions) {
+        let filename = url.split('/');
+        filename = filename[filename.length-1];
+        let filenameFile = path.parse(filename).name;
+        let filenameExtension = path.parse(filename).ext;
+        let baseUrlWithoutFilename = url.replace(filename, '');
+        let responsiveImage = baseUrlWithoutFilename + 'responsive/' + filenameFile + '-' + name + filenameExtension;
+        srcset.push(responsiveImage + ' ' + dimensionsData[name].width + 'w');
+    }
+
+    output = ' srcset="' + srcset.join(' ,') + '" ';
+
+    return new Handlebars.SafeString(output);
+}
+
+module.exports = {
+    responsiveSrcSetHelper,
+    returnSrcSetAttribute
+};
