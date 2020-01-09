0893f7edcd76757e5cd3e81418a9209beedfc814
Tomasz Dziuda <dziudek@gmail.com>
2020-01-09 21:35:48 +0100

    Removed unnecessary spellchecker package and added setTimeout to the preload script

--- app/back-end/app-preload.js
+++ app/back-end/app-preload.js
@@ -1,16 +1,19 @@
 const { SpellCheckHandler, ContextMenuListener, ContextMenuBuilder } = require('electron-spellchecker');
 const { remote } = require('electron');
 
-console.log('XXXXXXXXXXXXX');
-// const languageCode = remote.getGlobal('spellCheckerLanguage');
+setTimeout(() => {
+    console.log('XXXXXXXXXXXXX');
+    // const languageCode = remote.getGlobal('spellCheckerLanguage');
 
-// if (languageCode !== '') {
-    window.spellCheckHandler = new SpellCheckHandler();
-    window.spellCheckHandler.attachToInput();
-    window.spellCheckHandler.switchLanguage('en-US');
+    // if (languageCode !== '') {
+        window.spellCheckHandler = new SpellCheckHandler();
+        window.spellCheckHandler.attachToInput();
+        window.spellCheckHandler.switchLanguage('en-US');
 
-    let contextMenuBuilder = new ContextMenuBuilder(window.spellCheckHandler);
-    let contextMenuListener = new ContextMenuListener((info) => {
-        contextMenuBuilder.showPopupMenu(info);
-    });
-// } 
+        let contextMenuBuilder = new ContextMenuBuilder(window.spellCheckHandler);
+        let contextMenuListener = new ContextMenuListener((info) => {
+            contextMenuBuilder.showPopupMenu(info);
+        });
+    // } 
+}, 1000);
+
