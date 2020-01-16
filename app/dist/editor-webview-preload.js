bd0f283f9d7d269a7363090fcaf21071291b9bcd
Tomasz Dziuda <dziudek@gmail.com>
2020-01-16 20:55:36 +0100

    Handling spellchecker state for the block editor

--- app/dist/editor-webview-preload.js
+++ app/dist/editor-webview-preload.js
@@ -1,18 +1,2 @@
-/*
-const { SpellCheckHandler, ContextMenuListener, ContextMenuBuilder } = require('electron-spellchecker');
 const { remote } = require('electron');
-const languageCode = remote.getGlobal('spellCheckerLanguage');
-
-setTimeout(() => {
-    if (languageCode !== '') {
-        window.spellCheckHandler = new SpellCheckHandler();
-        window.spellCheckHandler.attachToInput();
-        window.spellCheckHandler.switchLanguage(languageCode);
-
-        let contextMenuBuilder = new ContextMenuBuilder(window.spellCheckHandler);
-        let contextMenuListener = new ContextMenuListener((info) => {
-            contextMenuBuilder.showPopupMenu(info);
-        });
-    } 
-}, 1000);
-*/
+window.spellCheckerIsActive = remote.getGlobal('spellCheckerIsActive');
