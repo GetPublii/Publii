d52ed59f1bfdc77f40849d57c3c73f81cb1beb3b
Tomasz Dziuda <dziudek@gmail.com>
2021-01-15 23:06:42 +0100

    fix: solve issues with noindex for pagination pages (#878)

--- app/back-end/modules/render-html/handlebars/helpers/meta-robots.js
+++ app/back-end/modules/render-html/handlebars/helpers/meta-robots.js
@@ -9,19 +9,9 @@ const Handlebars = require('handlebars');
  */
 function metaRobotsHelper(rendererInstance, Handlebars) {
     Handlebars.registerHelper('metaRobots', function (options) {
-        let output = '';
-
         // If canonical is set or AMP is currently rendered - skip meta robots tag
         if (rendererInstance.ampMode || options.data.root.hasCustomCanonicalUrl) {
             return '';
-        }
-
-        if (options.data.root.metaRobotsRaw === 'index, follow') {
-            return '';
-        }
-
-        if (options.data.root.metaRobotsRaw !== '') {
-            output = '<meta name="robots" content="' + options.data.root.metaRobotsRaw + '" />';
         }
 
         if (
@@ -39,10 +29,18 @@ function metaRobotsHelper(rendererInstance, Handlebars) {
                 )
             )
         ) {
-            output = '<meta name="robots" content="noindex, follow" />';
+            return new Handlebars.SafeString('<meta name="robots" content="noindex, follow" />');
         }
 
-        return new Handlebars.SafeString(output);
+        if (options.data.root.metaRobotsRaw === 'index, follow') {
+            return '';
+        }
+
+        if (options.data.root.metaRobotsRaw !== '') {
+            return new Handlebars.SafeString('<meta name="robots" content="' + options.data.root.metaRobotsRaw + '" />');
+        }
+
+        return '';
     });
 }
 
