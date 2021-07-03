1b20a2dff6e44c44a64c24a95b53441a2ce6b82c
Tomasz Dziuda <dziudek@gmail.com>
2021-07-03 22:41:07 +0200

    feat: removed pluralization rules as they are not required

--- app/src/main.js
+++ app/src/main.js
@@ -131,30 +131,6 @@ mainProcessAPI.receive('app-data-loaded', function (initialData) {
         locale: initialData.currentLanguage.name,
         messages: {
             [initialData.currentLanguage.name]: initialData.currentLanguage.translations
-        },
-        pluralizationRules: {
-            'pl': function (choice, choicesLength) {
-                if (choice === 0) {
-                    return 0;
-                }
-                
-                const teen = choice > 10 && choice < 20;
-                const endsWithOne = choice % 10 === 1;
-                
-                if (choicesLength < 4) {
-                    return (!teen && endsWithOne) ? 1 : 2;
-                }
-                
-                if (!teen && endsWithOne) {
-                    return 1;
-                }
-                
-                if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
-                    return 2;
-                }
-                
-                return (choicesLength < 4) ? 2 : 3;
-            }
         }
     });
 
