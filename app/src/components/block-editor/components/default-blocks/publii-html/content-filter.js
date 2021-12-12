6eac61ce81a90a7530cd46f28e4873c3cb2d6cc3
Tomasz Dziuda <tomasz@dziuda.com>
2021-12-12 08:30:28 +0100

    fix: solve issue with &nbsp; entities in the HTML block in the block editor

--- app/src/components/block-editor/components/default-blocks/publii-html/content-filter.js
+++ app/src/components/block-editor/components/default-blocks/publii-html/content-filter.js
@@ -2,14 +2,12 @@ function contentFilter (code, mode = 'editor') {
   if (mode === 'editor') {
     code = code.replace(/&gt;/gmi, '>');
     code = code.replace(/&lt;/gmi, '<');
-    code = code.replace(/&nbsp;/gmi, '&');
     code = code.replace(/<script/gmi, '<publii-script');
     code = code.replace(/<\/script/gmi, '</publii-script');
     code = code.replace(/<webview[\s\S]*?>[\s\S]*?<\/[\s\S]*?webview>/gmi, '');
   } else if (mode === 'render') {
     code = code.replace(/&gt;/gmi, '>');
     code = code.replace(/&lt;/gmi, '<');
-    code = code.replace(/&nbsp;/gmi, '&');
     code = code.replace(/<publii-script/gmi, '<script');
     code = code.replace(/<\/publii-script/gmi, '</script');
     code = code.replace(/<webview[\s\S]*?>[\s\S]*?<\/[\s\S]*?webview>/gmi, '');
