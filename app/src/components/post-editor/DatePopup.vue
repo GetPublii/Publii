c0836682b3cfad947c29f63711f9ecf8aa09ed20
Tomasz Dziuda <tomasz@dziuda.com>
2022-02-11 21:32:07 +0100

    feat: add native controls for date and time

--- app/src/components/post-editor/DatePopup.vue
+++ app/src/components/post-editor/DatePopup.vue
@@ -8,166 +8,12 @@
             </p>
 
             <div class="post-date-picker">
-                <select
-                    v-model="time.month"
-                    class="post-date-editor-month">
-                    <option value="1">{{ $t('date.jan') }}</option>
-                    <option value="2">{{ $t('date.feb') }}</option>
-                    <option value="3">{{ $t('date.mar') }}</option>
-                    <option value="4">{{ $t('date.apr') }}</option>
-                    <option value="5">{{ $t('date.may') }}</option>
-                    <option value="6">{{ $t('date.jun') }}</option>
-                    <option value="7">{{ $t('date.jul') }}</option>
-                    <option value="8">{{ $t('date.aug') }}</option>
-                    <option value="9">{{ $t('date.sep') }}</option>
-                    <option value="10">{{ $t('date.oct') }}</option>
-                    <option value="11">{{ $t('date.nov') }}</option>
-                    <option value="12">{{ $t('date.dec') }}</option>
-                </select>/
-
-                <select
-                    v-model="time.day"
-                    class="post-date-editor-day">
-                    <option value="1">01</option>
-                    <option value="2">02</option>
-                    <option value="3">03</option>
-                    <option value="4">04</option>
-                    <option value="5">05</option>
-                    <option value="6">06</option>
-                    <option value="7">07</option>
-                    <option value="8">08</option>
-                    <option value="9">09</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
-                </select>/
-
                 <input
-                    v-model="time.year"
-                    type="number"
-                    class="post-date-editor-year"
-                    step="1">
-
-                <select
-                    v-model="time.hours"
-                    class="post-date-editor-hours">
-                    <option value="0">00</option>
-                    <option value="1">01</option>
-                    <option value="2">02</option>
-                    <option value="3">03</option>
-                    <option value="4">04</option>
-                    <option value="5">05</option>
-                    <option value="6">06</option>
-                    <option value="7">07</option>
-                    <option value="8">08</option>
-                    <option value="9">09</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13" v-if="format24isActive">13</option>
-                    <option value="14" v-if="format24isActive">14</option>
-                    <option value="15" v-if="format24isActive">15</option>
-                    <option value="16" v-if="format24isActive">16</option>
-                    <option value="17" v-if="format24isActive">17</option>
-                    <option value="18" v-if="format24isActive">18</option>
-                    <option value="19" v-if="format24isActive">19</option>
-                    <option value="20" v-if="format24isActive">20</option>
-                    <option value="21" v-if="format24isActive">21</option>
-                    <option value="22" v-if="format24isActive">22</option>
-                    <option value="23" v-if="format24isActive">23</option>
-                </select>
-
-                :<select
-                    v-model="time.minutes"
-                    class="post-date-editor-minutes">
-                    <option value="0">00</option>
-                    <option value="1">01</option>
-                    <option value="2">02</option>
-                    <option value="3">03</option>
-                    <option value="4">04</option>
-                    <option value="5">05</option>
-                    <option value="6">06</option>
-                    <option value="7">07</option>
-                    <option value="8">08</option>
-                    <option value="9">09</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
-                    <option value="32">32</option>
-                    <option value="33">33</option>
-                    <option value="34">34</option>
-                    <option value="35">35</option>
-                    <option value="36">36</option>
-                    <option value="37">37</option>
-                    <option value="38">38</option>
-                    <option value="39">39</option>
-                    <option value="40">40</option>
-                    <option value="41">41</option>
-                    <option value="42">42</option>
-                    <option value="43">43</option>
-                    <option value="44">44</option>
-                    <option value="45">45</option>
-                    <option value="46">46</option>
-                    <option value="47">47</option>
-                    <option value="48">48</option>
-                    <option value="49">49</option>
-                    <option value="50">50</option>
-                    <option value="51">51</option>
-                    <option value="52">52</option>
-                    <option value="53">53</option>
-                    <option value="54">54</option>
-                    <option value="55">55</option>
-                    <option value="56">56</option>
-                    <option value="57">57</option>
-                    <option value="58">58</option>
-                    <option value="59">59</option>
-                </select>
-
-                <select
-                    v-if="!format24isActive"
-                    v-model="time.ampm"
-                    class="post-date-editor-ampm">
-                    <option value="am">AM</option>
-                    <option value="pm">PM</option>
-                </select>
+                    type="date"
+                    v-model="postDateTime.date" />
+                <input 
+                    type="time"
+                    v-model="postDateTime.time" />
             </div>
 
             <div class="buttons">
@@ -194,20 +40,11 @@ export default {
         return {
             isVisible: false,
             timestamp: 0,
-            time: {
-                month: 1,
-                day: 1,
-                year: 2016,
-                hours: 0,
-                minutes: 0,
-                ampm: 'am'
+            postDateTime: {
+                date: '',
+                time: ''
             }
         };
-    },
-    computed: {
-        format24isActive () {
-            return this.$store.state.app.config.timeFormat == 24;
-        }
     },
     mounted: function() {
         this.$bus.$on('date-popup-display', timestamp => {
@@ -235,45 +72,17 @@ export default {
         },
         calculateTimeFromTimestamp () {
             let date = new Date(this.timestamp);
-            this.time.month = date.getMonth() + 1;
-            this.time.day = date.getDate();
-            this.time.year = date.getFullYear();
-            this.time.hours = date.getHours();
-            this.time.minutes = date.getMinutes();
+            let month = date.getMonth() + 1;
+            let day = date.getDate();
+            let year = date.getFullYear();
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
 
-            if (!this.format24isActive) {
-                let ampmMode = date.toLocaleString('en-GB', { hour: 'numeric', hour12: true });
-                this.time.ampm = ampmMode.split(' ')[1];
-                this.time.hours = parseInt(ampmMode.split(' ')[0], 10);
-            }
+            this.postDateTime.date = [year, month, day].map(n => n < 10 ? '0' + n : n).join('-');
+            this.postDateTime.time = [hours, minutes].map(n => n < 10 ? '0' + n : n).join(':');
         },
         calculateTimestampFromTime () {
-            this.timestamp = new Date(
-                this.time.year,
-                this.time.month - 1,
-                this.time.day,
-                parseInt(this.convertHoursFrom12to24(), 10),
-                this.time.minutes
-            ).getTime();
-        },
-        convertHoursFrom12to24 () {
-            if (this.format24isActive) {
-                return this.time.hours;
-            }
-
-            if (this.time.ampm === 'pm') {
-                if (parseInt(this.time.hours, 10) === 12) {
-                    return 12;
-                }
-
-                return parseInt(this.time.hours, 10) + 12;
-            }
-
-            if (parseInt(this.time.hours, 10) === 12) {
-                return 0;
-            }
-
-            return this.time.hours;
+            this.timestamp = new Date(this.postDateTime.date + ' ' + this.postDateTime.time).getTime();
         }
     },
     beforeDestroy () {
