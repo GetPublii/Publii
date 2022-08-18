74280ebbea167cf40023041217ea85f7644f203c
Tomasz Dziuda <tomasz@dziuda.com>
2022-08-18 11:59:36 +0200

    fix: proper handling of publii.files.json during manual deployment (#2ufymd5)

--- app/back-end/modules/deploy/manual.js
+++ app/back-end/modules/deploy/manual.js
@@ -16,6 +16,7 @@ class ManualDeployment {
     async initConnection() {
         this.deployment.setInput();
         this.deployment.prepareLocalFilesList();
+        this.deployment.replaceSyncInfoFiles();
 
         switch(this.deployment.siteConfig.deployment.manual.output) {
             case 'catalog': this.returnCatalog(); break;
