4925d3fe09385ba5091006524b5ee65ab0cf4ec6
Tomasz Dziuda <tomasz@dziuda.com>
2022-10-18 20:41:56 +0200

    Revert "revert changes in site add screen"
    This reverts commit 118937f244a8b92026260ba305ff1b809e9d67e1.

--- app/src/components/basic-elements/Tabs.vue
+++ app/src/components/basic-elements/Tabs.vue
@@ -51,7 +51,8 @@ export default {
     },
     data () {
         return {
-            activeItem: false
+            activeItem: false,
+            activeIndex: 0
         }
     },
     mounted () {
@@ -59,6 +60,7 @@ export default {
 
         if(lastOpenedTab && this.items.indexOf(lastOpenedTab) > -1) {
             this.activeItem = lastOpenedTab;
+            this.activeIndex = this.items.indexOf(lastOpenedTab);
         } else {
             this.activeItem = this.items[0] || false;
         }
@@ -80,7 +82,12 @@ export default {
         },
         toggle (newActiveItem, scrollTo = false) {
             this.activeItem = newActiveItem;
-            window.sessionStorage.setItem(this.id, newActiveItem);
+            this.activeIndex = this.items.indexOf(newActiveItem);
+
+            if (this.id) {
+                window.sessionStorage.setItem(this.id, newActiveItem);
+            }
+
             this.onToggle();
 
             setTimeout(() => {
