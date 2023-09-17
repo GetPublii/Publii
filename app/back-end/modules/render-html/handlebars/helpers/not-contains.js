0acae8fef97fd0af8541537754e0d67099beab8c
Tomasz Dziuda <tomasz@dziuda.com>
2023-09-17 11:53:49 +0200

    feat: new handlebars helper - notContains (#8692r5c44)

---
+++ app/back-end/modules/render-html/handlebars/helpers/not-contains.js
@@ -0,0 +1,30 @@
+/**
+ * Helper for checking if a specifc value is not inside comma-separated string
+ *
+ * {{#notContains 'abc' 'abc,def'}}
+ *
+ * @returns {callback}
+ */
+function notContains (needle, haystack, options) {
+    if (needle === undefined || haystack === undefined) {
+        return false;
+    }
+
+    if (typeof haystack === 'object' && haystack.string) {
+        haystack = haystack.string;
+    }
+
+    haystack = haystack.split(',');
+
+    if (typeof needle === 'number') {
+        haystack = haystack.map(n => parseInt(n, 10));
+    }
+
+    if (haystack.indexOf(needle) === -1) {
+        return options.fn(this);
+    }
+
+    return options.inverse(this)
+}
+
+module.exports = notContains;
